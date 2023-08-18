4a973512f725b50db18ba0c48b03b956db117f56
soma-tepes <114878779+soma-tepes@users.noreply.github.com>
2023-08-17 19:34:41 -0700

    Update config.js

--- src/database/config.js
+++ src/database/config.js
@@ -7,7 +7,13 @@ const db = new Sequelize({
     database:'blog_m7nr',
     host:"dpg-cjfcek2nip6c73bke9n0-a.oregon-postgres.render.com",
     port:5432,
-    logging:false
+    logging:false,
+    dialectOptions: {
+    ssl: {
+      require: true, // Requiere conexión SSL/TLS
+      rejectUnauthorized: false, // Ignora los certificados autofirmados, utiliza un certificado CA válido en producción
+    },
+  },
 })
 
 module.exports = { db }
